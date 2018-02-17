a8fb3221be835d118dcc6ed064e267f8f1edfaab
ajayns <ajay.ns@outlook.com>
2018-02-17 16:16:35 +0530

    Add html.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,19 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins.  You can source data nodes from anywhere but
+     * most sites, like Gatsbygram, will include data from
+     * the filesystem so we start here with
+     * “gatsby-source-filesystem”.
+     *
+     * A site can have as many instances of
+     * gatsby-source-filesystem as you need.  Each plugin
+     * instance is configured with a root path where it then
+     * recursively reads in files and adds them to the data
+     * tree.
+     */
     'gatsby-source-hacker-news',
   ],
 };
---
+++ src/html.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+import favicon from "./assets/favicon.ico"
+
+class Html extends Component {
+    render() {
+        return (
+            <html op="news" lang="en">
+                <head>
+                    {this.props.headComponents}
+
+                    <meta name="referrer" content="origin" />
+                    <link rel="shortcut icon" href={favicon} />
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="description"
+                        content="Gatsby HackerNews: A clone of Hacker News written in Gatsby"
+                    />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0"
+                    />
+                    <title>Gatsby HackerNews</title>
+                </head>
+                <body>
+                    <div
+                        id="___gatsby"
+                        dangerouslySetInnerHTML={{ __html: this.props.body }}
+                    />
+                    {this.props.postBodyComponents}
+                </body>
+            </html>
+        )
+    }
+}
+
+module.exports = Html
