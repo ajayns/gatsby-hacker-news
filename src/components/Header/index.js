3f269b140c1a976b2cad7de039f83e9bcfd16137
ajayns <ajay.ns@outlook.com>
2018-02-18 12:06:22 +0530

    Add GH link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import github from "../../assets/github.png"
+
 const Header = () => (
   <div
     style={{
@@ -13,9 +15,15 @@ const Header = () => (
         margin: '0 auto',
         maxWidth: 960,
         padding: '1.45rem 1.0875rem',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ 
+        margin: 0,
+        display: 'inline', 
+        }}>
         <Link
           to="/"
           style={{
@@ -26,6 +34,17 @@ const Header = () => (
           Hacker News
         </Link>
       </h1>
+      <a
+          href="http://github.com/ajayns/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <img src={github} style={{ margin: 0, height: '28px' }}/>
+      </a>      
     </div>
   </div>
 )
